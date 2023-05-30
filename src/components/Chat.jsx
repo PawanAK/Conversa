5551bd880dcb36bedf5e4fc6aed7cd37fbc9669a
PawanAK <pawanajjark@gmail.com>
2023-05-30 11:41:42 +0530

    db config

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
+
 const Chat = () => {
-  return <div className="chat-app">Chat</div>;
+  const [newMessage, setNewMessage] = useState("");
+
+  const messagesRef = collection(db, "messages");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
+
+    await addDoc(messagesRef, {});
+  };
+
+  return (
+    <div className="chat-app">
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          type="text"
+          className="new-message-inut"
+          placeholder="Type your Message here"
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button className="send-button" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
